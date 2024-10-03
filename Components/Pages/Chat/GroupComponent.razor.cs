caafc16a6538f2f8d72a00254c7ad8e11fe4d6ae
JhonL2002 <jhonfredylopezarenas@gmail.com>
2024-10-03 16:42:33 -0500

    - Implementing a css grid to show all groups in one component

--- Components/Pages/Chat/GroupComponent.razor.cs
+++ Components/Pages/Chat/GroupComponent.razor.cs
@@ -1,57 +1,23 @@
-﻿using ClientChat.DTOs;
-using ClientChat.DTOs.Chats;
-using ClientChat.Responses;
+﻿using ClientChat.DTOs.Chats;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.JSInterop;
-using System.Security.Claims;
 
 namespace ClientChat.Components.Pages.Chat
 {
-    public partial class GroupComponent: ComponentBase
+    public partial class GroupComponent : ComponentBase
     {
-        private AuthenticationState _authenticationState;
-
-        public GroupDTO GroupDTO = new();
-
-        private string identifier;
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
+        private int selectedChat;
+        private bool isloading = true; //Initially, the component is loading
+        protected override async Task OnInitializedAsync()
         {
-            if (firstRender)
-            {
-                _authenticationState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-                var user = _authenticationState.User;
-                if (user.Identity is not null && user.Identity.IsAuthenticated)
-                {
-                    identifier = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
-                    GroupDTO.CreatorUserId = Convert.ToInt32(identifier);
-                }
-
-                StateHasChanged();
-            }
-
-            await manageGroupService.GetAllGroupsAsync();
-            StateHasChanged();
-        }
-        private async Task CreateGroup()
-        {
-            var isSuccess = await manageGroupService.CreateGroupAsync(GroupDTO);
-            if (isSuccess == true)
-            {
-                await js.InvokeVoidAsync("alert", "Group Created Successfully");
-                GroupDTO = new GroupDTO();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Error to create group!");
-            }
+            isloading = true; // Initialize loading
+            await userChat.GetUserChatsAsync();
+            isloading = false; // When chats are loaded, disable the load
         }
 
-        private async Task JoinToGroupButton(int chatId)
+        private void SelectChat(int chatId)
         {
-            await userChat.JoinToGroupAsync(chatId);
+            selectedChat = chatId;
+            NavigationManager.NavigateTo($"/chat/{chatId}");
         }
     }
 }
---
+++ Components/Pages/Chat/GroupPage.razor.cs
@@ -0,0 +1,57 @@
+﻿using ClientChat.DTOs;
+using ClientChat.DTOs.Chats;
+using ClientChat.Responses;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System.Security.Claims;
+
+namespace ClientChat.Components.Pages.Chat
+{
+    public partial class GroupPage: ComponentBase
+    {
+        private AuthenticationState _authenticationState;
+
+        public GroupDTO GroupDTO = new();
+
+        private string identifier;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _authenticationState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+                var user = _authenticationState.User;
+                if (user.Identity is not null && user.Identity.IsAuthenticated)
+                {
+                    identifier = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
+                    GroupDTO.CreatorUserId = Convert.ToInt32(identifier);
+                }
+
+                StateHasChanged();
+            }
+
+            await manageGroupService.GetAllGroupsAsync();
+            StateHasChanged();
+        }
+        private async Task CreateGroup()
+        {
+            var isSuccess = await manageGroupService.CreateGroupAsync(GroupDTO);
+            if (isSuccess == true)
+            {
+                await js.InvokeVoidAsync("alert", "Group Created Successfully");
+                GroupDTO = new GroupDTO();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error to create group!");
+            }
+        }
+
+        private async Task JoinToGroupButton(int chatId)
+        {
+            await userChat.JoinToGroupAsync(chatId);
+        }
+    }
+}
